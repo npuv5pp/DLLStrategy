14f72c0125c48a64d93f6a3fe1510d6c4b2ecbb9
AzureFx <i@azurefx.name>
2019-05-04 21:28:18 +0800

    Add log functions

--- DLLStrategy/DLLStrategy.cpp
+++ DLLStrategy/DLLStrategy.cpp
@@ -4,10 +4,12 @@
 #include "stdafx.h"
 #include "platform.h"
 #include "adapter.h"
-#include <cstdio>
+
 using namespace Simuro;
+using namespace Adapter;
 
 void OnEvent(EventType type, void* argument) {
+	SendLog(L"V/DLLStrategy:OnEvent()");
 	switch (type) {
 	case EventType::JudgeResult: {
 		JudgeResultEvent* judgeResult = static_cast<JudgeResultEvent*>(argument);
@@ -19,18 +21,21 @@ void OnEvent(EventType type, void* argument) {
 }
 
 void GetTeamInfo(TeamInfo* teamInfo) {
+	SendLog(L"V/DLLStrategy:GetTeamInfo()");
 	static const wchar_t teamName[] = L"公诚勇毅，永矢毋忘";
 	static constexpr size_t len = sizeof(teamName);
 	memcpy(teamInfo->teamName, teamName, len);
 }
 
 void GetInstruction(Field* field) {
+	SendLog(L"V/DLLStrategy:GetInstruction()");
 	for (auto& r : field->ourRobots) {
 		r.wheel = { 125,-125 };
 	}
 }
 
 void GetPlacement(Field* field) {
+	SendLog(L"V/DLLStrategy:GetPlacement()");
 	for (auto& r : field->ourRobots) {
 		r.position = { 42,42 };
 		r.rotation = 180;
--- DLLStrategy/adapter.h
+++ DLLStrategy/adapter.h
@@ -1,16 +1,65 @@
 #pragma once
-#include <cstdio>
+
+#include <string>
+/* If you want to use fmt, uncomment me
+#include <fmt/format.h>
+*/
 
 namespace Adapter {
-	void Dump(void* ptr, size_t len) {
-		char* buf = new char[len * 3 + 1];
-		buf[len * 3] = 0;
-		char* p = buf;
-		for (size_t i = 0; i < len; ++i) {
-			sprintf(p,"%02X ", ((char*)ptr)[i]);
-			p += 3;
+
+	/*
+	Sends a message to V5DLLAdapter.
+	lpMessage: The message text, in UTF-16LE encoding.
+	dwSizeInBytes: The message length in bytes. Does not include the null character.
+	*/
+	bool SendLog(LPCWSTR lpMessage, DWORD dwSizeInBytes) {
+		static struct {
+			HWND hWnd = nullptr;
+			DWORD dwMyPid = GetCurrentProcessId();
+		} params;
+		static const DWORD ADAPTER_MAGIC = 0x56352B2B;
+		static const DWORD DATA_LOG = 1;
+		if (params.hWnd == nullptr) {
+			auto enumProc = [](HWND hWnd, LPARAM lParam)->BOOL {
+				auto& p = *(decltype(params)*)lParam;
+				DWORD dwTargetPid = 0;
+				GetWindowThreadProcessId(hWnd, &dwTargetPid);
+				if (dwTargetPid == p.dwMyPid) {
+					LONG magic = GetWindowLongW(hWnd, GWLP_USERDATA);
+					if (magic == ADAPTER_MAGIC) {
+						p.hWnd = hWnd;
+						return FALSE;
+					}
+				}
+				return TRUE;
+			};
+			EnumWindows((WNDENUMPROC)enumProc, (LPARAM)&params);
 		}
-		MessageBoxA(0, buf, 0, 0);
-		delete[] buf;
+		if (params.hWnd != nullptr) {
+			COPYDATASTRUCT cds;
+			cds.dwData = DATA_LOG;
+			cds.cbData = dwSizeInBytes;
+			cds.lpData = (LPVOID)lpMessage;
+			return SendMessageW(params.hWnd, WM_COPYDATA, NULL, (LPARAM)&cds);
+		}
+		return false;
 	}
+
+	//You are encouraged to create your own wrapper for this.
+
+	/*
+	Sends a message to V5DLLAdapter.
+	message: The message text.
+	*/
+	bool SendLog(const std::wstring& message) {
+		return SendLog(message.c_str(), message.size() * sizeof(wchar_t));
+	}
+
+	/* If you want to use fmt, uncomment me
+	template<typename ...Args>
+	bool Log(fmt::WCStringRef format, Args&& ...args) {
+		auto str = fmt::format(format, std::forward<Args>(args)...);
+		return SendLog(str.c_str(), str.size() * sizeof(decltype(str)::value_type));
+	}
+	*/
 }
